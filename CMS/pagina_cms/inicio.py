cb9414df029b4e5fe39624c5c05a43cdca676d7f
sebasgfb <segbasgfb@gmail.com>
2024-06-05 18:02:34 -0400

    xd

--- CMS/pagina_cms/inicio.py
+++ CMS/pagina_cms/inicio.py
@@ -5,40 +5,48 @@ def inicio(request):
     return render(request, 'inicio.html')
 
 def acceder(request):
-    if request.method=='GET':
+    if request.method == 'GET':
         if 'codigo_usuario' in request.session:
-        #if request.session['codigo_usuario']>0:
-            variables={}
-            variables['nombre_usuario']= request.session['nombre_usuario']
-            return render(request,'panel.html', variables)
+            variables = {}
+            variables['nombre_usuario'] = request.session['nombre_usuario']
+            return render(request, 'panel.html', variables)
         else:
-            return render(request,'acceder.html')
+            return render(request, 'acceder.html')
     
-    if request.method=='POST':
-        v_usuario=request.POST.get('usuario')
-        v_clave=request.POST.get('clave')
-        verificar_usuario=usuarios.objects.filter(usuario=v_usuario, estado='ACTIVO')
-        print(verificar_usuario)
-        variables={}
-        if verificar_usuario.count()>0:
-            if verificar_usuario[0].clave==v_clave:
-                request.session['codigo_usuario']=verificar_usuario[0].id
-                request.session['nombre_usuario']=verificar_usuario[0].nombre
-                request.session['nivel_usuario']=verificar_usuario[0].nivel
-                variables['nombre_usuario']= request.session['nombre_usuario']
-                return render(request,'panel.html', variables)
+    if request.method == 'POST':
+        v_usuario = request.POST.get('usuario')
+        v_clave = request.POST.get('clave')
+        verificar_usuario = usuarios.objects.filter(usuario=v_usuario, estado='ACTIVO')
+        variables = {}
+        if verificar_usuario.count() > 0:
+            if verificar_usuario[0].clave == v_clave:
+                request.session['codigo_usuario'] = verificar_usuario[0].id
+                request.session['nombre_usuario'] = verificar_usuario[0].nombre
+                request.session['nivel_usuario'] = verificar_usuario[0].nivel
+                variables['nombre_usuario'] = request.session['nombre_usuario']
+
+                # Redirigir según el nivel del usuario
+                nivel_usuario = verificar_usuario[0].nivel
+                if nivel_usuario == 'LECTOR':
+                    return redirect('/ver_noticias')
+                elif nivel_usuario in ['MODERADOR', 'ADMINISTRADOR']:
+                    return redirect('/acceder')
+                else:
+                    # Manejar cualquier otro nivel no esperado
+                    variables['m_error'] = 'Nivel de usuario no reconocido'
+                    return render(request, 'acceder.html', variables)
             else:
-                variables['m_error']='La contraseña es incorrecta'
-                return render (request, 'acceder.html', variables)
+                variables['m_error'] = 'La contraseña es incorrecta'
+                return render(request, 'acceder.html', variables)
         else:
-            variables['m_error']='El usuario no esta registrado'
-            return render (request, 'acceder.html', variables)
+            variables['m_error'] = 'El usuario no está registrado'
+            return render(request, 'acceder.html', variables)
 
 def salir(request):
-    #request.session['codigo_usuario']=0
-    #request.session['nombre_usuario']=''
-    #request.session['nivel_usuario']=''
-    del request.session['codigo_usuario']
-    del request.session['nombre_usuario']
-    del request.session['nivel_usuario'] 
+    if 'codigo_usuario' in request.session:
+        del request.session['codigo_usuario']
+    if 'nombre_usuario' in request.session:
+        del request.session['nombre_usuario']
+    if 'nivel_usuario' in request.session:
+        del request.session['nivel_usuario']
     return redirect('inicio')
